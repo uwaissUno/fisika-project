36a0759a31d240f3b681ec103a102b8be788b517
uwaissUno <149289804+uwaissUno@users.noreply.github.com>
2024-01-17 16:06:21 +0700

    Ngebuat hero section responsif

--- src/app/api/name/route.ts
+++ src/app/api/name/route.ts
@@ -1,19 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest , res : NextResponse) {
+export async function GET( res : NextResponse) {
   const members = [
-    { id: 1, name: "rana jihan s" },
-    { id: 2, name: "citra hadi n" },
-    { id: 3, name: "aisyatul khumairoh" },
-    { id: 4, name: "naurah calista ramadhani" },
-    { id: 5, name: "hiyasatakunu a.a.m" },
-    { id: 6, name: "athifa zaina anggraeni" },
-    { id: 7, name: "devia salsabila" },
-    { id: 8, name: "hita tuhu f" },
-    { id: 9, name: "Moch ridho zulqornain" },
-    { id: 10, name: "naufal arga rizky pratama" },
-    { id: 11, name: "fawwaz safaraz n" },
-    { id: 12, name: "M. Uwaiss Uno Maliki" },
-  ];
+    { "id": 1, "name": "Rana Jihan S." },
+    { "id": 2, "name": "Citra Hadi N." },
+    { "id": 3, "name": "Aisyatul Khumairoh" , "absen" : 1, "image" : "/image/tutul.jpg" },
+    { "id": 4, "name": "Naurah Calista Ramadhani" , "absen" : 21},
+    { "id": 5, "name": "Hiyasatakunu A.A.M" , "absen" : 13},
+    { "id": 6, "name": "Athifa Zaina Anggraeni" },
+    { "id": 7, "name": "Devia Salsabila" },
+    { "id": 8, "name": "Hita Tuhu F." },
+    {
+      "id": 9,
+      "name": "Moch Ridho Zulqornain",
+      "image": "https://i.postimg.cc/LswpYkht/Whats-App-Image-2024-01-16-at-13-50-26.jpg"
+    },
+    { "id": 10, "name": "Naufal Arga Rizky Pratama", "absen": 20 },
+    { "id": 11, "name": "Fawwaz Safaraz n" },
+    { "id": 12, "name": "M. Uwaiss Uno Maliki", "absen": 18 },
+    { "id": 13, "name": "Kayla Dwiky S." }
+  ]
+  
   return NextResponse.json({status : 200 , message : 'ok', data : members});
 }
